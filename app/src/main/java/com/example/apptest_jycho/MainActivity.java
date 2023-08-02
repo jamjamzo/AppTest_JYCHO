e61df994636e8cf58b73e39b3a8a520cc4591817
jamjamzo <jaeyoun92@gmail.com>
2023-08-02 17:31:16 +0900

    My Activity 추가 및 입력/종료 버튼 생성

--- app/src/main/java/com/example/apptest_jycho/MainActivity.java
+++ app/src/main/java/com/example/apptest_jycho/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.apptest_jycho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText)findViewById(R.id.input_Et);
+        Button button = (Button)findViewById(R.id.input_Btn);
+        TextView textView = (TextView)findViewById(R.id.output_Tv);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+                textView.setText(text);
+            }
+        });
+
+        Button actBtn = findViewById(R.id.activity_launch_Btn);
+        actBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MyActivity.class);
+                startActivities(i);
+            }
+        });
     }
-}
+
+
+
+};
+
+
+
+
